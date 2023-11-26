f38dde0e3f48581f96296ec6d6d5b9c15596739f
izzudd <36723840+izzudd@users.noreply.github.com>
2023-11-26 21:04:17 +0700

    implement authentication middleware

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/RPL-Cuci-in-Kelompok6/cuciin-backend/db"
+	"github.com/RPL-Cuci-in-Kelompok6/cuciin-backend/middleware"
 	"github.com/RPL-Cuci-in-Kelompok6/cuciin-backend/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -20,6 +21,15 @@ func main() {
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "%s", "Pong")
+	})
+	r.GET("/secret", middleware.Authenticate, func(c *gin.Context) {
+		c.String(http.StatusOK, "%s", "You are authorized")
+	})
+	r.GET("/secret/customer", middleware.Authenticate, middleware.Authorize(routes.ROLE_CUSTOMER), func(c *gin.Context) {
+		c.String(http.StatusOK, "%s", "You are authorized as customer")
+	})
+	r.GET("/secret/mitra", middleware.Authenticate, middleware.Authorize(routes.ROLE_PARTNER), func(c *gin.Context) {
+		c.String(http.StatusOK, "%s", "You are authorized as mitra")
 	})
 
 	r.POST("/customer/register", routes.RegisterUser(true))
